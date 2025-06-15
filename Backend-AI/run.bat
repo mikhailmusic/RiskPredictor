9b0cd83eabdd278023682c637f460785ecc931ff
Mikhail Alekseev <mishaalekseev2004@mail.ru>
2025-06-15 11:28:31 +0300

    update run script

--- Backend-AI/run.bat
+++ Backend-AI/run.bat
@@ -7,12 +7,17 @@ set APP_PATH=src\app.py
 if not exist %VENV_DIR%\Scripts\activate (
     echo [INFO] Creating virtual environment...
     python -m venv %VENV_DIR%
+
+    echo [INFO] Activating virtual environment...
     call %VENV_DIR%\Scripts\activate
 
+    echo [INFO] Upgrading pip...
+    %VENV_DIR%\Scripts\python.exe -m pip install --upgrade pip
+
     echo [INFO] Installing dependencies from requirements.txt...
-    pip install --upgrade pip >nul
-    pip install -r requirements.txt
+    %VENV_DIR%\Scripts\python.exe -m pip install -r requirements.txt
 ) else (
+    echo [INFO] Activating virtual environment...
     call %VENV_DIR%\Scripts\activate
 )
 
